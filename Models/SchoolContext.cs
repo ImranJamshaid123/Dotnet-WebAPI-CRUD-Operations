07b54849ee0fa3dc8ff47c5836f207c19507591a
Imran Jamshaid <imranjamshaid534@gmail.com>
2024-11-23 23:07:45 -0800

    First Commit

---
+++ Models/SchoolContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Models;
+
+public partial class SchoolContext : DbContext
+{
+    public SchoolContext()
+    {
+
+    }
+
+    public SchoolContext(DbContextOptions<SchoolContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; } = null;
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC2775A96D59");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
